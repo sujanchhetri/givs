548c6f5bd7ee69f613b1c25e60448736884b0cb7
Sujan Chhetri <sujanxchhetri@gmail.com>
2022-06-14 15:20:10 +0545

    initial

---
+++ faq/assets/js/script.js
@@ -0,0 +1,8 @@
+
+      $(document).ready(function(){
+        $('.menu-hamburger').click(function(){
+          $('.menu-hamburger').toggleClass('active')
+          $('.menu').toggleClass('active')
+        });
+      });
+
